5f73270d4fce90192851c59d63cd0ea516eb1c06
ezra.gordon <ezra.gordon@nexog.com>
2021-07-15 00:14:22 +0300

    add webpack for bundling and development server

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+var path = require('path')
+
+module.exports = {
+	mode: 'development',
+	output: {
+		path: path.resolve(__dirname, "public/dist")
+	},
+	module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  },
+	devServer: {
+		contentBase: path.join(__dirname, 'public'),
+		port: 9000
+	}
+}
