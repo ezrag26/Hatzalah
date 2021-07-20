06e77793ab578596336ec86585518b34cb44079f
ezra.gordon <ezra.gordon@nexog.com>
2021-07-20 23:46:04 +0300

    add header with alert banner on top, side menu on smaller screens. Nav items get bunched up for right now, and there is no search ability. Temporary ellipses is used to open the side menu, but should be changed in the future with a hamburger button

---
+++ src/header.js
@@ -0,0 +1,79 @@
+import React, { useState } from 'react'
+
+const Nav = () => {
+	return (
+		<nav>
+			<ul className={'flex'}>
+				{
+					[{ display: 'In Case of Emergency', href: '/in-case-of-emergency' }, { display: 'Who We Are', href: '/who-we-are' }, { display: 'Training & Resources', href: '/training-resources' }, { display: 'Donate', href: '/donate' }].map(headerItem => {
+						return <li className="header-nav-item"><a href={`${headerItem.href}`}>{headerItem.display}</a></li>
+					})
+				}
+			</ul>
+		</nav>
+	)
+}
+
+const calcTotalElemHeight = (elem) => {
+	if (!elem) return ''
+	const contentPlusPadding = elem.clientHeight
+	const style = getComputedStyle(elem)
+
+	const marginTop = parseInt(style.marginTop)
+	const marginBottom = parseInt(style.marginBottom)
+	const borderTopWidth = parseInt(style.borderTopWidth) || 0
+	const borderBottomWidth = parseInt(style.borderBottomWidth) || 0
+
+	return contentPlusPadding + marginTop + marginBottom + borderTopWidth + borderBottomWidth
+}
+
+const CurtainMenu = ({ open, setOpen }) => {
+	return (
+		<div id={'curtain'} style={{ top: calcTotalElemHeight(document.getElementById('alert-banner')) }} className={`flex ${open ? 'open' : ''}`}>
+			<Nav />
+		</div>
+	)
+}
+
+const Header = () => {
+	const SMALL_DISPLAY_WIDTH = 475
+	const [open, setOpen] = useState(false)
+	const [isSmallDisplay, setIsSmallDisplay] = useState(window.innerWidth < SMALL_DISPLAY_WIDTH)
+
+	window.addEventListener('resize', () => {
+		if (window.innerWidth > SMALL_DISPLAY_WIDTH && isSmallDisplay) {
+			setIsSmallDisplay(false)
+			setOpen(false)
+		} else if (window.innerWidth < SMALL_DISPLAY_WIDTH && !isSmallDisplay) {
+			setIsSmallDisplay(true)
+		}
+	})
+
+	return (
+		<header>
+			<div id={'alert-banner'}>
+				<p>We are hard at work getting ready to launch in the community. Stay tuned for more information!</p>
+				<button className={'button'}>Support Us</button>
+			</div>
+			<div id={'header'} className={`flex flex-row align-center ${open ? 'open' : ''}`}>
+			{isSmallDisplay &&
+				<>
+					<div id={'hamburger'} className={`${open ? 'open' : ''}`} onClick={e => {
+						setOpen(prev => !prev)
+					}}>...</div>
+					<CurtainMenu open={open} setOpen={setOpen} />
+				</>
+			}
+				<div id={'header-content'} className={`flex flex-row align-center`}>
+					<div className={'flex flex-row align-center'}>
+						<img src="/assets/hatzalah-logo-transparent.png" style={{ width: '40px', height: '40px' }} alt={'hatzalah-wol-logo'}/>
+						<div>Hatzalah of West Orange and Livingston</div>
+					</div>
+					{!isSmallDisplay && <Nav />}
+				</div>
+			</div>
+		</header>
+	)
+}
+
+export default Header
--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+import Header from './header'
+
 const App = () => {
 	return (
 		<>
+			<Header />
 		</>
 	)
 }
