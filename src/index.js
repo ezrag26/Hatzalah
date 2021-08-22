0f936a40de432a1283e10862570be1a9b2bf141f
ezra.gordon <ezra.gordon@nexog.com>
2021-08-22 20:59:41 +0300

    remove unecessary App component. Header and Footer are render into two different areas of the page, not through App

--- src/index.js
+++ src/index.js
@@ -4,14 +4,5 @@ import ReactDOM from 'react-dom'
 import Header from './header'
 import Footer from './footer'
 
-const App = () => {
-	return (
-		<>
-			<Header />
-			<Footer />
-		</>
-	)
-}
-
 ReactDOM.render(<Header />, document.querySelector('header'))
 ReactDOM.render(<Footer />, document.querySelector('footer'))
